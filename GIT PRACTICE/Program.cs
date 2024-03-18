133f9dadb9ba713055da5e4b43a868f7a16085ce
ummerfarooqshah <suhaanumer07@gmail.com>
2024-03-18 11:38:25 +0530

    test

--- GIT PRACTICE/Program.cs
+++ GIT PRACTICE/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int i = 10;
+            for (i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
